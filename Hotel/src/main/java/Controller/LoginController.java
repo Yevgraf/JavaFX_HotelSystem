89fce66593eef778688177911293e9c67f0522c0
Ricardo Rocheta <1210512@isep.ipp.pt>
2022-11-29 23:54:16 +0000

    FXML feito para Login

---
+++ Hotel/src/main/java/Controller/LoginController.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private PasswordField passwordTxt;
+
+    @FXML
+    private TextField userTxt;
+
+    @FXML
+    void clickLoginBtn(ActionEvent event) {
+
+    }
+
+}
