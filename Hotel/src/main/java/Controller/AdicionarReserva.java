9c6a8255944f06efc620093241bd61fbf3fbb8c3
RafaelMagalhães <rafael.cmagalhaes99@gmail.com>
2022-12-01 17:46:54 +0000

    Ao inserir reserva o quarto passa para Ativo=true e não aparece como opção nos quartos.

--- Hotel/src/main/java/Controller/AdicionarReserva.java
+++ Hotel/src/main/java/Controller/AdicionarReserva.java
@@ -90,33 +90,40 @@ public class AdicionarReserva implements Initializable {
 
         MessageBoxes.ShowMessage(Alert.AlertType.CONFIRMATION,"Comfirmar reserva","Deseja criar esta reserva?");
         PreparedStatement ps2;
+        PreparedStatement ps3;
         try {
             DBconn dbConn = new DBconn();
             Connection connection = dbConn.getConn();
             ps2 = connection.prepareStatement("INSERT INTO Reserva(nifCliente, idColaborador, idQuarto," +
                     "dataInicio, dataFim, servExtra, preco)" +
                     "VALUES (?,?,?,?,?,?,?)", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+
             ps2.setInt(1, cmbClientes.getValue().getNif());
             ps2.setInt(2, idColadorador);
-
             ps2.setInt(3, cmbIDQuarto.getValue().getId());
+
+
             ps2.setString(4, DatePickerInicio.getValue().toString());
             ps2.setString(5, DatePickerFim.getValue().toString());
             ps2.setString(6, servExtras);
             ps2.setDouble(7, preco);
             ps2.executeUpdate();
+            ps3 = connection.prepareStatement("UPDATE Quarto SET ativo=? WHERE id=?");
+            ps3.setBoolean(1,true);
+            ps3.setInt(2,cmbIDQuarto.getValue().getId().intValue());
+            ps3.executeUpdate();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
     private void initCombos() {
-
         for (int i = 0; i < Quarto.getQuarto().size(); i++) {
-            if (Quarto.getQuarto().get(i).getAtivo() == false) ;
-            cmbIDQuarto.getItems().addAll(Quarto.getQuarto());
+            if (Quarto.getQuarto().get(i).getAtivo() == false){
+             Quarto quartoFalse = Quarto.getQuarto().get(i);
+                cmbIDQuarto.getItems().addAll(quartoFalse);
+            }
         }
-
         cmbClientes.getItems().addAll(Cliente.getCliente());
     }
 
