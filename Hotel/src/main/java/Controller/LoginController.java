cadd0c0e43ef331531e660c7dce574007b319611
Ricardo Rocheta <1210512@isep.ipp.pt>
2022-12-08 14:34:35 +0000

    login

--- Hotel/src/main/java/Controller/LoginController.java
+++ Hotel/src/main/java/Controller/LoginController.java
@@ -3,6 +3,7 @@ package Controller;
 import BLL.UserBLL;
 import DAL.DBconn;
 import DAL.UserDAL;
+import Model.MessageBoxes;
 import Model.User;
 import com.example.hotel.Main;
 import javafx.event.ActionEvent;
@@ -38,11 +39,11 @@ public class LoginController implements Initializable {
         if (userTxt.getText().isEmpty() == false && passwordTxt.getText().isEmpty() == false) {
             Login();
         } else {
-            EmptyMessage.setText("Preencha todos os campos");
+            MessageBoxes.ShowMessage(Alert.AlertType.INFORMATION, "Preencha todos os campos","Informação");
         }
     }
 
-    void Login(){
+    void Login() {
         try {
             UserBLL bll = new UserBLL();
             User user = bll.Login(userTxt.getText(), passwordTxt.getText());
@@ -57,7 +58,7 @@ public class LoginController implements Initializable {
                     newStage.hide();
                     stage.setScene(new Scene(fxmlLoader.load()));
                     stage.show();
-                } else if (user.getTipoUser().getTipo().equals("Funcionario")){
+                } else if (user.getTipoUser().getTipo().equals("Funcionario")) {
                     FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("PainelFuncionario.fxml"));
                     Stage stage = new Stage();
                     Stage newStage = (Stage) loginBtn.getScene().getWindow();
@@ -65,39 +66,47 @@ public class LoginController implements Initializable {
                     newStage.hide();
                     stage.setScene(new Scene(fxmlLoader.load()));
                     stage.show();
+     //Inserir Login pág CLIENTE
+               // } else if (user.getTipoUser().getTipo().equals("Cliente")) {
+                 //  FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("PainelFuncionario.fxml"));
+                 //  Stage stage = new Stage();
+                 //  Stage newStage = (Stage) loginBtn.getScene().getWindow();
+                 //  stage.setTitle("Página do Cliente");
+                 //  newStage.hide();
+                 //  stage.setScene(new Scene(fxmlLoader.load()));
+                 //  stage.show();
                 }
-            }
-            else {
-                // LOGIN FALHOU
+            } else {
+                MessageBoxes.ShowMessage(Alert.AlertType.ERROR, "Login incorreto!","Erro");
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-   //public boolean VerifyUser() {
-   //        boolean flag;
-   //        String verificar = "SELECT count(1) FROM Colaborador WHERE utilizador ='" + userTxt.getText() + "'";
-   //        try {
-   //            PreparedStatement stmt = DBconn.getConn().prepareStatement(verificar);
-   //            ResultSet rs = stmt.executeQuery();
-   //            while (rs.next()) {
-   //                if (rs.getInt(1) == 0) {
-   //                    ValidarUser.setText("Utilizador não encontrado!");
-   //                } else {
-   //                    ValidarUser.setText("");
-   //                }
-   //            }
-   //        } catch (SQLException e) {
-   //            e.getCause();
-   //        }
-   //        if (ValidarUser.getText().equals("Utilizador não encontrado!")) {
-   //            flag = false;
-   //        } else {
-   //            flag = true;
-   //        }
-   //        return flag;
-   //    }
+    //public boolean VerifyUser() {
+    //        boolean flag;
+    //        String verificar = "SELECT count(1) FROM Colaborador WHERE utilizador ='" + userTxt.getText() + "'";
+    //        try {
+    //            PreparedStatement stmt = DBconn.getConn().prepareStatement(verificar);
+    //            ResultSet rs = stmt.executeQuery();
+    //            while (rs.next()) {
+    //                if (rs.getInt(1) == 0) {
+    //                    ValidarUser.setText("Utilizador não encontrado!");
+    //                } else {
+    //                    ValidarUser.setText("");
+    //                }
+    //            }
+    //        } catch (SQLException e) {
+    //            e.getCause();
+    //        }
+    //        if (ValidarUser.getText().equals("Utilizador não encontrado!")) {
+    //            flag = false;
+    //        } else {
+    //            flag = true;
+    //        }
+    //        return flag;
+    //    }
 
     /*public boolean VerifyPass() {
         boolean flag;
