291144ba7c7153fcdc6436df3eb9ece39b6ab770
Ricardo Rocheta <1210512@isep.ipp.pt>
2023-01-27 01:04:58 +0000

    preparaçao para Tickets

---
+++ Hotel/src/main/java/Model/EstacionamentoAPI/TicketInfo.java
@@ -0,0 +1,13 @@
+package Model.EstacionamentoAPI;
+
+import java.util.Date;
+
+public class TicketInfo {
+    public String GroupId;
+    public String ClientId;
+    public String LicencePlate;
+    public Date StartDate;
+    public Date EndDate;
+    public String ParkingSpot;
+    public Boolean Active;
+}
