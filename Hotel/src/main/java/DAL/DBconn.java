9c700346430693084f453a321c50255a21cc8d38
Yevgraf <118963244+Yevgraf@users.noreply.github.com>
2023-12-29 11:06:28 +0000

    removida conexão da db

--- Hotel/src/main/java/DAL/DBconn.java
+++ Hotel/src/main/java/DAL/DBconn.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DBconn {
 
-    private static String url = "jdbc:sqlserver://ctespbd.dei.isep.ipp.pt;databaseName=2022_F_LP3_G4;TrustServerCertificate=True";
-    private static String user = "2022_F_LP3_G4";
-    private static String pass = "123+qwe*123";
+    private static String url = "";
+    private static String user = "";
+    private static String pass = "";
 
     /**
      * A função getConn serve para fazer a conexão á base de dados
